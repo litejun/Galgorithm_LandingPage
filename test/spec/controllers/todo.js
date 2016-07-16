08f5fce06bdf12b34bd075c5144b49072679641e
litejun <litejun24@gamil.com>
2016-07-17 01:27:36 +0900

    Lesson 1 Success

--- test/spec/controllers/todo.js
+++ test/spec/controllers/todo.js
@@ -18,6 +18,6 @@ describe('Controller: TodoCtrl', function () {
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(TodoCtrl.awesomeThings.length).toBe(3);
+    expect(scope.todos.length).toBe(0);
   });
 });
